7a769513151ddb58d4a55f254215e8c1fad26c0d
Seijo Cecilia <willamwei@outlook.com>
2025-03-13 20:03:38 +0800

    [koishi-adapter] redefine format

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from 'koishi';
 
-export const name = 'adapter-tanebi'
+export const name = 'adapter-tanebi';
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // write your plugin here
+    // write your plugin here
 }
