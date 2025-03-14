8e228f7524b0189a02e8c98bceb93a2916c72126
Wesley F. Young <willamwei@outlook.com>
2025-03-14 19:11:11 +0800

    [koishi-adapter] put refresh logic in `finally`

--- src/index.ts
+++ src/index.ts
@@ -80,23 +80,26 @@ class TanebiBot extends KoishiBot<Context, TanebiBot.Config> {
         this.internal.onWarning((module, message) => this.logger.warn(`[${module}] ${message}`));
 
         if (firstUse) {
-            await this.internal.qrCodeLogin((url, png) => {
-                this.logger.info('Please scan the QR code on the screen with Mobile QQ.');
-                this.logger.info('Or you can manually generate a QR code with the following URL:');
-                this.logger.info(url);
-                this.qrCode = png;
+            try {
+                await this.internal.qrCodeLogin((url, png) => {
+                    this.logger.info('Please scan the QR code on the screen with Mobile QQ.');
+                    this.logger.info('Or you can manually generate a QR code with the following URL:');
+                    this.logger.info(url);
+                    this.qrCode = png;
+                    this.ctx.console.refresh('tanebi.qrcode');
+                });
+                await this.ctx.database.create('tanebi.deviceInfo', {
+                    uin: this.config.uin,
+                    payload: JSON.stringify(serializeDeviceInfo(this.internal[ctx].deviceInfo)),
+                });
+                await this.ctx.database.create('tanebi.keystore', {
+                    uin: this.config.uin,
+                    payload: JSON.stringify(serializeKeystore(this.internal[ctx].keystore)),
+                });
+            } finally {
+                delete this.qrCode;
                 this.ctx.console.refresh('tanebi.qrcode');
-            });
-            delete this.qrCode;
-            this.ctx.console.refresh('tanebi.qrcode');
-            await this.ctx.database.create('tanebi.deviceInfo', {
-                uin: this.config.uin,
-                payload: JSON.stringify(serializeDeviceInfo(this.internal[ctx].deviceInfo)),
-            });
-            await this.ctx.database.create('tanebi.keystore', {
-                uin: this.config.uin,
-                payload: JSON.stringify(serializeKeystore(this.internal[ctx].keystore)),
-            });
+            }
         } else {
             await this.internal.fastLogin();
         }
